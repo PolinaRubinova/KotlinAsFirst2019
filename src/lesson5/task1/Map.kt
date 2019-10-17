e06a78f9aec1b936a638596b691d3840485dbdb2
rubinova.pv <jerrygonsales@mail.ru>
2019-10-17 19:14:57 +0300

    парам пам пам

--- src/lesson5/task1/Map.kt
+++ src/lesson5/task1/Map.kt
@@ -405,6 +405,7 @@ fun propagateHandshakes(friends: Map<String, Set<String>>): Map<String, Set<Stri
  *   findSumOfTwo(listOf(1, 2, 3), 4) -> Pair(0, 2)
  *   findSumOfTwo(listOf(1, 2, 3), 6) -> Pair(-1, -1)
  */
+
 fun findSumOfTwo(list: List<Int>, number: Int): Pair<Int, Int> {
     for (i in 0 until list.size) {
         for (j in i + 1 until list.size) {
